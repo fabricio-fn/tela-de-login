cb1ab8da7b7e031ee0f9a898139a903e77153b88
fabricio-fn <fabriciocunha040@gmail.com>
2023-02-07 17:06:13 -0300

    adicionei modo dark

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 // Vou fazer algumas funcionalidades depois
+
+// Modo dark
+
+const modoescuro = document.getElementById('mododark');
+
+modoescuro.addEventListener('click', () => {
+    const mudarparadark = document.getElementById('formulario');
+    if (modoescuro.classList.contains('bi-moon-fill')) {
+        modoescuro.classList.remove('bi-moon-fill');
+        modoescuro.classList.add('bi-sun-fill');
+
+        mudarparadark.classList.add('dark');
+    } else {
+        modoescuro.classList.add('bi-moon-fill');
+        modoescuro.classList.remove('bi-sun-fill');
+        mudarparadark.classList.remove('dark');
+    }
+});
